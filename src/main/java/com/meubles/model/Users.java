7eb6136c4f22edfd4b84dc1354efe0f99ec4b3f6
Alix VEYRAT <alixveyrat@gmail.com>
2025-11-04 15:27:05 +0100

    feat: add User entity with JPA annotations

---
+++ src/main/java/com/meubles/model/Users.java
@@ -0,0 +1,34 @@
+package com.meubles.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column
+    private String address;
+}
+
+
+
